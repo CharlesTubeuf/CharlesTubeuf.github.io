a76619548f1ca19a3f1a20759085170c5c4f8cf6
CharlesTubeuf <charles.tubeuf@hotmail.fr>
2023-06-23 18:55:26 +0200

    skill section completed and some design changes

--- app.js
+++ app.js
@@ -27,24 +27,24 @@ document.addEventListener('click', function (event) {
 if (window.addEventListener("load", () => {
         var typed = new Typed('#description', {
             strings: [
-                "Bonjour à tous et à toutes,<br/> ^700 diplômé d'une <strong>école d'ingénieur</strong>" +
-                " en biologie je suis tombé par hasard dans le <strong>développement web</strong>.<br/> ^800" +
+                "Bonjour à tous et à toutes,<br/> ^700 diplômé d'une <strong style='color: #01b2aa;'>école d'ingénieur</strong>" +
+                " en biologie je suis tombé par hasard dans le <strong style='color: #01b2aa;'>développement web</strong>.<br/> ^800" +
                 " Rapidement captivé par cette discipline, j'ai décidé d'en faire mon métier.<br/> ^800" +
-                " Après plusieurs formations et <strong> une année et demie d'alternance</strong>,^800 je suis actuellement à la recherche" +
-                " d'une entreprise pour développer mes compétences et exprimer ma <strong>créativité</strong>.<br/>^800",
+                " Après plusieurs formations et <strong style='color: #01b2aa;'> une année et demie d'alternance</strong>,^800 je suis actuellement à la recherche" +
+                " d'une entreprise pour développer mes compétences et exprimer ma <strong style='color: #01b2aa;'>créativité</strong>.<br/>^800",
 
-                "Bonjour à tous et à toutes,<br/>  diplômé d'une <strong>école d'ingénieur</strong>" +
-                " en biologie je suis tombé par hasard dans le <strong>développement web</strong>.<br/> " +
+                "Bonjour à tous et à toutes,<br/>  diplômé d'une <strong style='color: #01b2aa;'>école d'ingénieur</strong>" +
+                " en biologie je suis tombé par hasard dans le <strong style='color: #01b2aa;'>développement web</strong>.<br/> " +
                 " Rapidement captivé par cette discipline, j'ai décidé d'en faire mon métier.<br/> " +
-                " Après plusieurs formations et <strong> une année et demie d'alternance</strong>, je suis actuellement à la recherche" +
-                " d'une entreprise pour développer mes compétences et exprimer ma <strong>créativité</strong>.<br/>" +
+                " Après plusieurs formations et <strong style='color: #01b2aa;'> une année et demie d'alternance</strong>, je suis actuellement à la recherche" +
+                " d'une entreprise pour développer mes compétences et exprimer ma <strong style='color: #01b2aa;'>créativité</strong>.<br/>" +
                 " Cette animation est réalisée via la library typed.js, merci Matt Boldt.^2000",
 
-                "Bonjour à tous et à toutes,<br/>  diplômé d'une <strong>école d'ingénieur</strong>" +
-                " en biologie je suis tombé par hasard dans le <strong>développement web</strong>.<br/> " +
+                "Bonjour à tous et à toutes,<br/>  diplômé d'une <strong style='color: #01b2aa;'>école d'ingénieur</strong>" +
+                " en biologie je suis tombé par hasard dans le <strong style='color: #01b2aa;'>développement web</strong>.<br/> " +
                 " Rapidement captivé par cette discipline, j'ai décidé d'en faire mon métier.<br/> " +
-                " Après plusieurs formations et <strong> une année et demie d'alternance</strong>, je suis actuellement à la recherche" +
-                " d'une entreprise pour développer mes compétences et exprimer ma <strong>créativité</strong>.<br/>" +
+                " Après plusieurs formations et <strong style='color: #01b2aa;'> une année et demie d'alternance</strong>, je suis actuellement à la recherche" +
+                " d'une entreprise pour développer mes compétences et exprimer ma <strong style='color: #01b2aa;'>créativité</strong>.<br/>" +
                 " Bienvenu sur mon cv !",
             ],
             typeSpeed: 10,
@@ -56,13 +56,6 @@ if (window.addEventListener("load", () => {
         });
     }));
 
-function random($min, $max) {
-    return random() * ($max - $min) + $min;
-}
-
-function randomColor() {
-    return random(0, 16777215).floor().toNumber().toString(16);
-}
 //#endregion
 
 //#region                   SCROLL ANIMATION
@@ -70,16 +63,60 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } 
-    })
-})
+            if (entry.target.classList.contains('skill')) {
 
+                var rounds = entry.target.querySelectorAll(".round");
+                rounds.forEach(round => {
+                    var roundCircle = round.querySelector("circle");
+                    var roundRadius = roundCircle.getAttribute("r");
+                    var roundPercent = round.dataset.percent;
+                    var roundCircum = 2 * roundRadius * Math.PI;
+                    var roundDraw = (roundPercent * roundCircum) / 100;
+                    round.style.strokeDasharray = roundDraw + " 9999";
+                });
+
+            }
+        }
+    });
+});
 const hiddenElements = document.querySelectorAll('.hiddenLeft , .hiddenRight');
 hiddenElements.forEach((el) => observer.observe(el));
+
 //#endregion
 
-//#region                   3D animation
+//#region                   SKILLS CIRCLE
+
+// function DrawRound(entry) {
+//     document.addEventListener("DOMContentLoaded", () => {
+//         var rounds = entry.querySelectorAll(".round");
+//         rounds.forEach(round => {
+//             var roundCircle = round.querySelector("circle");
+//             var roundRadius = roundCircle.getAttribute("r");
+//             var roundPercent = round.dataset.percent;
+//             var roundCircum = 2 * roundRadius * Math.PI;
+//             var roundDraw = (roundPercent * roundCircum) / 100;
+//             round.style.strokeDasharray = roundDraw + " 99999";
+//         })
+//     })
+
+// };
 
 
+// document.addEventListener("DOMContentLoaded", function() {
+//     var rounds = document.querySelectorAll(".round");
+//     rounds.forEach(round => {
+//         var roundCircle = round.querySelector("circle");
+//         var roundRadius = roundCircle.getAttribute("r");
+//         var roundPercent = round.dataset.percent;
+//         var roundCircum = 2 * roundRadius * Math.PI;
+//         var roundDraw = (roundPercent * roundCircum) / 100;
+//         round.style.strokeDasharray = roundDraw + " 9999";
+//     })
+//   });
 
+
+//#endregion  
+
+
+//#region                   background animation
 //#endregion         
