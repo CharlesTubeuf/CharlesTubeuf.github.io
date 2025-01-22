9331be0c18b6b2cbf333548f5890ee79678a744d
CharlesTubeuf <charles.tubeuf@hotmail.fr>
2025-01-22 12:15:38 +0100

    Modification du texte d'introduction

--- app.js
+++ app.js
@@ -27,18 +27,16 @@ if (
   window.addEventListener("load", () => {
     var typed = new Typed("#description", {
       strings: [
-        "Bonjour à tous et à toutes,<br/> ^700 diplômé d'une <strong style='color: #01b2aa;'>école d'ingénieur</strong>" +
+        "Bonjour à toutes et à tous,<br/> ^700 diplômé d'une <strong style='color: #01b2aa;'>école d'ingénieur</strong>" +
           " en biologie j'ai découvert le <strong style='color: #01b2aa;'>développement web</strong>.<br/> ^800" +
           " Rapidement captivé par cette discipline, j'ai décidé d'en faire mon métier.<br/> ^800" +
-          " Après plusieurs formations et <strong style='color: #01b2aa;'> une année et demie d'alternance</strong>,^800 je suis actuellement à la recherche" +
-          " d'une entreprise pour développer mes compétences et exprimer ma <strong style='color: #01b2aa;'>créativité</strong>.<br/>^800",
-
-        "Bonjour à tous et à toutes,<br/>  diplômé d'une <strong style='color: #01b2aa;'>école d'ingénieur</strong>" +
-          " en biologie j'ai découvert le <strong style='color: #01b2aa;'>développement web</strong>.<br/> " +
-          " Rapidement captivé par cette discipline, j'ai décidé d'en faire mon métier.<br/> " +
-          " Après plusieurs formations et <strong style='color: #01b2aa;'> une année et demie d'alternance</strong>, je suis actuellement à la recherche" +
-          " d'une entreprise pour développer mes compétences et exprimer ma <strong style='color: #01b2aa;'>créativité</strong>.<br/>" +
-          " Bienvenu sur mon cv !",
+          " Après plusieurs formations et <strong style='color: #01b2aa;'> quelques expériences professionnelles</strong>,^800" + 
+          " j'ai découvert une réelle appétence pour l'<strong style='color: #01b2aa;'>UI/UX</strong> et la satisfaction" +
+          " de produire du <strong style='color: #01b2aa;'>clean code</strong>. <br/> ^800" + 
+          " A mon arrivé sur un projet je n'hésite pas a de poser des questions et je suis ravi de pouvoir aider à mon tour !<br/> ^800" +
+          " Je souhaite intégrer une entreprise qui valorise le <strong style='color: #01b2aa;'>craftsmanship</strong> ^500" + 
+          " et <strong style='color: #01b2aa;'>la veille documentaire</strong> afin de favoriser l'excellence et l'évolution de ses collaborateurs.<br/> ^800" +
+          " Bienvenu sur mon cv !"
       ],
       typeSpeed: 5,
       backSpeed: 20,
